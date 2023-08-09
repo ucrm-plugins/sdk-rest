e559d35a79aca76ab6199c0c0b98746fcaf71216
Ryan Spaeth <rspaeth@spaethtech.com>
2023-08-09 03:20:38 -0500

    Finished initial work on EndpointBuilder

--- src/REST/Endpoints/Country.php
+++ src/REST/Endpoints/Country.php
@@ -1,41 +1,41 @@
-<?php /** @noinspection PhpUnused */
-declare(strict_types=1);
-
-namespace SpaethTech\UCRM\SDK\REST\Endpoints;
-
-//use SpaethTech\UCRM\SDK\REST\Endpoints\EndpointObject;
-//use SpaethTech\UCRM\SDK\REST\Annotations\CachedAnnotation as Cached;
-//use SpaethTech\UCRM\SDK\REST\Annotations\EndpointAnnotation as Endpoint;
-
-//use UCRM\REST\Endpoints\Helpers\CountryHelper;
-
-/**
- * Class Country
- *
- * @Cached
- *
- * @Endpoint { "get": "/countries", "getById": "/countries/:id" }
- *
- * @method string|null getName()
- * @method string|null getCode()
- *
- * @package UCRM\REST\Endpoints
- * @author Ryan Spaeth <rspaeth@spaethtech.com>
- * @copyright Copyright (c) 2020 - Spaeth Technologies Inc.
- * @final
- */
-final class Country extends EndpointObject
-{
-    use CountryHelper;
-
-    /**
-     * @var string
-     */
-    protected string $name;
-
-    /**
-     * @var string
-     */
-    protected string $code;
-
-}
+<?php ///** @noinspection PhpUnused */
+//declare(strict_types=1);
+//
+//namespace SpaethTech\UCRM\SDK\REST\Endpoints;
+//
+////use SpaethTech\UCRM\SDK\REST\Endpoints\EndpointObject;
+////use SpaethTech\UCRM\SDK\REST\Annotations\CachedAnnotation as Cached;
+////use SpaethTech\UCRM\SDK\REST\Annotations\EndpointAnnotation as Endpoint;
+//
+////use UCRM\REST\Endpoints\Helpers\CountryHelper;
+//
+///**
+// * Class Country
+// *
+// * @Cached
+// *
+// * @Endpoint { "get": "/countries", "getById": "/countries/:id" }
+// *
+// * @method string|null getName()
+// * @method string|null getCode()
+// *
+// * @package UCRM\REST\Endpoints
+// * @author Ryan Spaeth <rspaeth@spaethtech.com>
+// * @copyright Copyright (c) 2020 - Spaeth Technologies Inc.
+// * @final
+// */
+//final class Country extends EndpointObject
+//{
+//    use CountryHelper;
+//
+//    /**
+//     * @var string
+//     */
+//    protected string $name;
+//
+//    /**
+//     * @var string
+//     */
+//    protected string $code;
+//
+//}
